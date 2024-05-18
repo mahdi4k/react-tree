0e4a5bad6b8d9ded71a763d50ca2017a7375baf9
mft <falahati.mahdi@yahoo.com>
2024-05-18 11:08:21 +0330

    fix issue build

--- buildWindows.bat
+++ buildWindows.bat
@@ -3,6 +3,7 @@ REM This has to be in a separate batch file rather than a sequence of ... && ...
 REM "set" command only takes effect AFTER the line containing it has completed. Chaining it with &&
 REM means it will NOT take effect for the commands following it in the same line.
 set NODE_ENV=production
+set NODE_OPTIONS=--openssl-legacy-provider
 IF EXIST .\\js ( rmdir /S /Q .\\js )
 REM "tsc" and "yarn" are batch files, so unless we use "call" to execute them, execution won't come back here.
 call tsc -p tsconfig.build.json
